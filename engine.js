5f7b34cfd96d97dce48c0a4f1409e023c39c43a4
peter <im7mortal@gmail.com>
2014-10-08 09:05:17 +0400

    add carousel

--- engine.js
+++ engine.js
@@ -2,77 +2,67 @@ angular.module('skill', [
 	'ctrl',
 	'xc.indexedDB'
 ]).config(function ($indexedDBProvider) {
+
 	$indexedDBProvider.connection('zhmotDB')
 		.upgradeDatabase('myVersion', function(event, db, tx){
+			console.log('lol');
+
 			var objStore = db.createObjectStore('money', {keyPath: 'id'});
 			objStore.createIndex('amount_idx', 'amount', {unique: false});
 		}
 	);
-});
+})	.service('zhmotDB', ['xc.indexedDB', function() {
+	this.OBJECT_STORE_NAME = 'money';
+	this.myObjectStore = $indexedDB.objectStore(OBJECT_STORE_NAME);
 
-angular.module('ctrl', []).controller('myControllerName', function($scope, $indexedDB) {
+	this.initData = function () {
+		return myObjectStore.getAll().then(function(results) {
+			return results;
+		});
+	};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*		$scope.objects = [];
-
-		var OBJECT_STORE_NAME = 'money';
-
-		var myObjectStore = $indexedDB.objectStore(OBJECT_STORE_NAME);
-
-		//$scope.addWaste = function () {
-		//
-		//};
-
-
-		myObjectStore.insert({"ssn": "444-444-562-110","name": "John Doich", "age": 57}).then(function(e){
+	this.insert = function (name, amount) {
+		var id = (Math.random() * 10000000000000000).toString();
+		this.myObjectStore.insert({"id": id,"name": name, "amount": amount}).then(function(e){
 			console.log('g')
 		});
+	}
 
-		myObjectStore.getAll().then(function(results) {
-			// Update scope
-			$scope.objects = results;
-		});*/
 
-		/**
-		 * execute a query:
-		 * presuming we've an index on 'age' field called 'age_idx'
-		 * find all persons older than 40 years
-		 */
 
-/*		var myQuery = $indexedDB.queryBuilder().$index('age_idx').$gt(40).$asc.compile();
-		myObjectStore.each(myQuery).then(function(cursor){
-			cursor.key;
-			cursor.value;
 
-		});*/
-/*		var i = 0;
-		$scope.fight = function () {
-			i++;
-			console.log(i);
-			myObjectStore.insert({"ssn": i,"name": "John Doich", "age": 57}).then(function(e){
-				console.log('g')
-			});
-		};
-		$scope.all = function () {
-			myObjectStore.getAll().then(function (data) {
-				console.log(data)
-			});
+	//		var myQuery = $indexedDB.queryBuilder().$index('age_idx').$gt(40).$asc.compile();
+	//myObjectStore.each(myQuery).then(function(cursor){
+	//cursor.key;
+	//cursor.value;
+	//
+	//});
 
-		}*/
+
+}]);;
+
+angular.module('ctrl', []).controller('myControllerName', function($scope, zhmotDB) {
+
+
+
+$scope.add = function (name, amount) {
+	zhmotDB.insert(name, amount)
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	});
